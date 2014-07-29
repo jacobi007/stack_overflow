cf11aabf251be4ffb178b7ddeba695564c16a3df
jacobi007 <jk.kubiak@gmail.com>
2014-07-29 22:56:12 +0200

    create profile page-not yet finished

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Project::Application.routes.draw do
   resources :answers
   root 'questions#index'
   resources :questions
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
