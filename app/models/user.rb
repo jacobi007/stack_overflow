61b5e17ea75e282c4f9772bd7e6404028eaa460a
jacobi007 <jk.kubiak@gmail.com>
2014-07-30 22:10:16 +0200

    add ranking feature to profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User < ActiveRecord::Base
   has_many :questions
   has_many :answers, through: :questions
 
-  has_attached_file :avatar, :styles => { :top_user => "40x40>", :test_profile => "169x169>", :thumb => "80x80>", :profile => "170x170>" },
-   :default_url => "http://lorempixel.com/120/80/abstract/"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  default_scope order('points DESC')
 
+  has_attached_file :avatar, :styles => { :top_user => "40x40>", :test_profile => "169x169>",
+                                          :thumb => "80x80>", :profile => "170x170>" },
+                    :default_url => "http://lorempixel.com/120/80/abstract/"
+
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
