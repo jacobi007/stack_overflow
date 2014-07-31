5268ffe762dacd04452b36b3f3675c2fd01a17ec
jacobi007 <jk.kubiak@gmail.com>
2014-07-31 18:09:11 +0200

    repair migrations

--- db/migrate/20140727122256_create_answers.rb
+++ db/migrate/20140727122256_create_answers.rb
@@ -2,8 +2,7 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.text :body
-      t.references :question
-
+      
       t.timestamps
     end
   end
